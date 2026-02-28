876e3d6e11908cd903593948d616e6814cefede7
Ethan Hawksley <ethan@hawksley.dev>
2026-02-28 12:08:59 +0000

    atoheu

--- entity_spawner.gd
+++ entity_spawner.gd
@@ -4,6 +4,10 @@ func _process(delta: float) -> void:
 	global.time_left_until_spawn -= delta
 	if global.time_left_until_spawn <= 0:
 		global.time_left_until_spawn += global.time_per_spawn
+		spawn_entity()
 		
 	if delta > randf():
 		print("thing")
+
+func spawn_entity() -> void:
+	
