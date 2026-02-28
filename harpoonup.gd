a27b66ab23719769fa0c2f46bf3a5a676646662d
Ethan Hawksley <ethan@hawksley.dev>
2026-02-28 18:12:39 +0000

    bought

--- harpoonup.gd
+++ harpoonup.gd
@@ -1,7 +1,7 @@
 extends Label
 
 func _process(_delta) -> void:
-	text="Harpoon"+"\n"+"£"+str((global.inflation*75))+" Brought:"+str(int(global.harpoonup))
+	text="Harpoon"+"\n"+"£"+str((global.inflation*75))+" Bought:"+str(int(global.harpoonup))
 
 	if global.mode != global.modes.shop:
 		text = ""
