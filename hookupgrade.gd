777d67100755b1e6d96e541034dd4a4b166a4976
samrexlord4 <sam_08_merritt@icloud.com>
2026-02-28 12:12:03 +0000

    global war

--- hookupgrade.gd
+++ hookupgrade.gd
@@ -6,7 +6,14 @@ func _ready() -> void:
 	pressed.connect(hookup)
 	pass # Replace with function body.
 func hookup():
-	print("hookup")
+	if global.money >= global.inflation * 100:
+		global.money = global.money - global.inflation * 100
+		global.catches = global.catches + 1
+		global.inflation = global.inflation * 1.1
+		global.money = floor(global.money)
+		global.inflation = ceil(global.inflation)
+		print(global.inflation)
+		print(global.money)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
--- spppppppppeeeeeeeeeeeeeddddddddddd.gd
+++ spppppppppeeeeeeeeeeeeeddddddddddd.gd
@@ -3,8 +3,17 @@ extends TextureButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pressed.connect(boatsboost)
+	pass # pressed.connect(rodup)Replace with function body.
+func boatsboost():
+	if global.money >= global.inflation * 100:
+		global.money = global.money - global.inflation * 100
+		global.speed = global.speed + 0.025
+		global.inflation = global.inflation * 1.1
+		global.money = floor(global.money)
+		global.inflation = ceil(global.inflation)
+		print(global.inflation)
+		print(global.money)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
