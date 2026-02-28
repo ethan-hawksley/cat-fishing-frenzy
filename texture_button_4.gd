34d454df2a3ecf88c3ff5f5b4d10408e01caa685
samrexlord4 <sam_08_merritt@icloud.com>
2026-02-28 16:34:42 +0000

    buttons

--- texture_button_4.gd
+++ texture_button_4.gd
@@ -6,14 +6,13 @@ func _ready() -> void:
 	pressed.connect(harpoonkill)
 	pass # Replace with function body.
 func harpoonkill():
-	if global.money >= global.inflation * 100:
-		global.money = global.money - global.inflation * 100
-		global.harpspeed = global.harpspeed * 0.5
-		global.inflation = global.inflation * 1.1
-		global.money = floor(global.money)
-		global.inflation = ceil(global.inflation)
-		print(global.inflation)
-		print(global.money)
+	if global.harptime != 0:
+		if global.money >= global.inflation * 100:
+			global.money = global.money - global.inflation * 100
+			global.harptime = global.harptime - 0.5
+			global.inflation = global.inflation * 1.1
+			global.money = floor(global.money)
+			global.inflation = ceil(global.inflation)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
