15eabd71b9b6b66197c2787ee3960dc1880fb033
samrexlord4 <sam_08_merritt@icloud.com>
2026-02-28 13:27:27 +0000

    jj

--- texture_button_4.gd
+++ texture_button_4.gd
@@ -3,8 +3,17 @@ extends TextureButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	pressed.connect(harpoonkill)
 	pass # Replace with function body.
-
+func harpoonkill():
+	if global.money >= global.inflation * 100:
+		global.money = global.money - global.inflation * 100
+		global.harpspeed = global.harpspeed * 0.5
+		global.inflation = global.inflation * 1.1
+		global.money = floor(global.money)
+		global.inflation = ceil(global.inflation)
+		print(global.inflation)
+		print(global.money)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- texture_button_5.gd
+++ texture_button_5.gd
@@ -3,8 +3,17 @@ extends TextureButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	pressed.connect(morefish)
 	pass # Replace with function body.
-
+func morefish():
+	if global.money >= global.inflation * 100:
+		global.money = global.money - global.inflation * 100
+		global.time_per_spawn = global.time_per_spawn + 0.05
+		global.inflation = global.inflation * 1.1
+		global.money = floor(global.money)
+		global.inflation = ceil(global.inflation)
+		print(global.inflation)
+		print(global.money)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
