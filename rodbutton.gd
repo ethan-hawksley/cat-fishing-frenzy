edd7994c480017e22503a931d761e945b253ff93
samrexlord4 <sam_08_merritt@icloud.com>
2026-02-28 11:31:03 +0000

    a lot of hard work

--- rodbutton.gd
+++ rodbutton.gd
@@ -1,12 +1,16 @@
-extends PopupMenu
+extends TextureButton
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	pressed.connect(rodup)
 	pass # Replace with function body.
-
+	
+func rodup():
+	print("button rod pressed")
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+
+func _process(_delta: float) -> void:
 	pass
