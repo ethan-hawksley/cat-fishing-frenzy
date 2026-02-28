1675992743d7fc4ef365ac87b8fbe1dd9a43793b
Ethan Hawksley <ethan@hawksley.dev>
2026-02-28 12:03:50 +0000

    spawning

--- entity_spawner.gd
+++ entity_spawner.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 func _process(delta: float) -> void:
-	global.time_until_spawn -= global.spawn_s
+	global.time_left_until_spawn -= delta
+	if global.time_left_until_spawn <= 0:
+		global.time_left_until_spawn += global.time_per_spawn
+		
 	if delta > randf():
 		print("thing")
--- global.gd
+++ global.gd
@@ -11,6 +11,9 @@ var speed = 0.1
 var mode = modes.shop
 var inflation = 1
 
+var time_per_spawn = 1
+var time_left_until_spawn = 0
+
 enum modes {
 	descending,
 	ascending,
