f3ed6e6c856433749b8ca0f7e2dca9f58a7f3a0a
samrexlord4 <sam_08_merritt@icloud.com>
2026-03-08 15:29:46 +0000

    prices

--- texture_button_7.gd
+++ texture_button_7.gd
@@ -7,16 +7,16 @@ func _ready() -> void:
 	pass # Replace with function body.
 func lesssharks():
 	if global.sharkchance < 0.95:
-		if global.money >= global.inflationshark * 50:
-			global.money = global.money - global.inflationshark * 50
+		if global.money >= global.inflationshark * 500:
+			global.money = global.money - global.inflationshark * 500
 			global.sharkchance = global.sharkchance * 1.025
-			global.inflationshark = global.inflationshark * 1.1
+			global.inflationshark = global.inflationshark * 5
 			global.money = snapped(global.money,0.01)
 			global.lesssharkss += 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if global.money < global.inflationshark * 50 or global.lesssharkss == 5:
+	if global.money < global.inflationshark * 500 or global.lesssharkss == 5:
 		disabled = true
 	modulate = Color.WHITE if not disabled else Color(1, 1, 1, 0.4)
 	pass
