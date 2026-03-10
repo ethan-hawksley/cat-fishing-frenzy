f6095d48ee224018c0255553e4b1874c059ca64e
samrexlord4 <sam_08_merritt@icloud.com>
2026-03-10 14:56:44 +0000

    ducks

--- duck_2.gd
+++ duck_2.gd
@@ -3,15 +3,15 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	position.x = 100
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.y = 5*sin(Time.get_ticks_msec()/250 + 1)
-	position.x += -0.5*cos(Time.get_ticks_msec()/250 + 0.5)
-	position.x += 0.1 
-	
-	if position.x > 230:
+	position.y = 1*sin(Time.get_ticks_msec()/250 + 1)
+	position.x += -0.6*cos(Time.get_ticks_msec()/250 + 0.5)
+	position.x += 0.2 
+	if position.x > 330:
 		position.x = -430
-		
+	pass
