b7ed66cd65da63f81005e069b008444db19839f2
RyleyDoesGaming <ryleymjackson@gmail.com>
2026-03-11 19:16:44 +0000

    reallignment

--- fish_1.gd
+++ fish_1.gd
@@ -8,4 +8,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if global.collection[0]:
+		show()
+	else:
+		hide()
