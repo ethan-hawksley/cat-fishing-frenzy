fd1c5e959fd577d49b7f91590483f6f064787aff
RyleyDoesGaming <ryleymjackson@gmail.com>
2026-03-11 16:59:05 +0000

    Revert "g"
    This reverts commit 9f5b432fe31552a8abdf4130f4b846670775ba80.

--- collection.gd
+++
@@ -1,9 +0,0 @@
-extends CanvasLayer
-
-@export var collection: Control
-
-func _process(delta: float) -> void:
-	if global.mode == global.modes.collection:
-		collection.show()
-	else:
-		collection.hide()
