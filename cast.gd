469807bb13245d72e1adf6f38c16ab6b19b01e17
Ethan Hawksley <ethan@hawksley.dev>
2026-03-11 09:38:37 +0000

    some empty files removed

--- cast.gd
+++ cast.gd
@@ -16,24 +16,20 @@ func _process(delta: float) -> void:
 		if Input.is_action_pressed("ui_right"):
 			position.x += (global.speed*1200 * delta)
 	if global.mode == global.modes.ascending:
-		position.y -= (global.speed*600 * delta)
 		if position.x < -64:
-			position.x += (global.speed*1200 * delta)
+			position.x += (global.speed * 1200 * delta)
 		if position.x > -64:
-			position.x -= (global.speed*1200 * delta)
+			position.x -= (global.speed * 1200 * delta)
+			
 		if position.y > 0:
-			position.y -= (global.speed*600 * delta)
+			position.y -= (global.speed * 600 * delta)
 		else:
 			global.mode = global.modes.shop
 			global.money += global.value_of_reeled_fish
 			global.value_of_reeled_fish = 0
 			global.caught_fish = 0
 			position.y = 0
-		if position.x < -64:
-			position.x += (global.speed*1200 * delta)
-		if position.x > -64:
-			position.x -= (global.speed*1200 * delta)
-
+			position.x = -64
 	if global.mode == global.modes.shop:
 		$Camera2D.enabled = true
 		position.x = -64
