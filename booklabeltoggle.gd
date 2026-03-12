22a4b10a6237834a18c201a00093d4ee8f2bad8b
Ethan Hawksley <ethan@hawksley.dev>
2026-03-12 21:54:18 +0000

    implement beastiary

--- booklabeltoggle.gd
+++ booklabeltoggle.gd
@@ -8,6 +8,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	text = "Collection\nBook ->"
-	if global.mode != global.modes.shop:
-		text = ""
+	if global.mode == global.modes.shop or global.mode == global.modes.collection:
+		show()
+	else:
+		hide()
