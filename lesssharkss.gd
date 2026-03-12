686f9fbf861138eb30cd8fba04b6e700008ce9c9
RyleyDoesGaming <ryleymjackson@gmail.com>
2026-03-12 12:33:32 +0000

    fixed text boxes

--- lesssharkss.gd
+++ lesssharkss.gd
@@ -8,7 +8,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	text="less sharks\n£"+str(snapped((global.inflationshark*500),0.01))+"\nBought:"+str(int(global.lesssharkss))
+	text="Less Sharks\n£"+str(snapped((global.inflationshark*500),0.01))+"\nBought:"+str(int(global.lesssharkss))
 	if global.mode != global.modes.shop:
 		text = ""
 	
