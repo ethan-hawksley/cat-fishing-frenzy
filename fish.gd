9a5171dac7c18f0911ebcf4d832fcba6c110d0b5
RyleyDoesGaming <ryleymjackson@gmail.com>
2026-03-15 16:11:18 +0000

    fixed shocks again

--- fish.gd
+++ fish.gd
@@ -232,7 +232,7 @@ func add_collection(fish_type: String) -> void:
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if not area.is_in_group("hook"):
 		return
-		
+		 
 	if jellyfish == false:
 		if not caught:
 			AudioManager.play_sfx("catch")
